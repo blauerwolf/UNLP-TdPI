f19f01584d6daeee5b0d643b933d4212ee5bced0
Ernesto Ardenghi <eardenghi@gmail.com>
2024-08-21 16:49:36 -0300

    Ej2

--- resol_ej2.pas
+++ resol_ej2.pas
@@ -12,29 +12,134 @@
 }
 
 program ej2;
-uses crt, sysutils;
+uses sysutils;
 
-procedure TableHeader();
-begin
-  writeln('      '#201);
-  writeln('  Dia '#186);
-  writeln('Cod.P '#186);
-  writeln(' Cant '#186);
-  writeln('      '#200);
+Const 
+  dimF = 300;
+
+Type 
+  oficina = record 
+    codigo: integer;
+    dni: LongInt;
+    expensa: real;
+  end;
+
+  rango = 1..dimF;
+  vector = array[1..dimF] of oficina;
+
+procedure LeerOficina(var o: oficina);
+begin 
+  write('Codigo: '#9); readln(o.codigo);
+  if (o.codigo <> -1) then begin
+    write('DNI: '#9,#9); readln(o.dni);
+    write('Valor Expensa: '#9); readln(o.expensa);
+  end;
+  writeln;
 end;
 
-procedure TableFooter();
-begin
-  //gotoXY(WhereX + 7, WhereY - 5);
-  writeln(Char(187));
-  writeln(Char(186));
-  writeln(Char(186));
-  writeln(Char(186));
-  writeln(Char(188));
+procedure CargarPagos(var v: vector; var dimL: rango);
+var o: oficina;
+begin 
+    // No controlo si dimL < dimF porque por def es menor o igual
+    LeerOficina(o);
+    while (o.codigo <> -1) and (dimL < dimF) do begin
+      dimL := dimL + 1;
+      v[dimL] := o;
+
+      LeerOficina(o);
+    end;
 end;
 
-begin
-  TableHeader();
-  TableFooter();
+procedure sort_insercion(var v: vector; dimL: rango);
+var 
+    i, j: rango;
+    actual: oficina;
 
-end.
+begin 
+    for i := 2 to dimL do begin
+        actual := v[i];
+        j := i - 1;
+
+        while (j > 0) and (v[j].codigo > actual.codigo) do 
+        begin 
+            v[j + 1] := v[j];
+            j := j - 1;
+        end;
+
+        v[j + 1] := actual;
+    end;
+end;
+
+procedure sort_seleccion(var v: vector; var dimL: rango);
+var 
+    i, j, pos: rango;
+    item: oficina;
+begin 
+    { busca el mínimo y guarda en pos la posición }
+    for i := 1 to dimL - 1 do begin 
+        pos := i;
+        for j := i + 1 to dimL do 
+            if v[j].codigo < v[pos].codigo then pos := j;
+
+        { intercambia v[i] y v[p]}
+        item := v[pos];
+        v[pos] := v[i];
+        v[i] := item;
+    end;
+end;
+
+procedure ImprimirVector(v: vector; dimL: rango);
+var 
+  i, j, k, longDni: integer;
+begin 
+  writeln(' Codigo |    DNI    | Expensas ');
+  writeln('-------------------------------');
+  for i := 1 to dimL do begin 
+    if (v[i].codigo < 9) then 
+      write('   00')
+    else if (v[i].codigo < 100) then
+      write('   0')
+    else write('   ');
+
+    write(v[i].codigo, '   ');
+
+    longDni := Length(IntToStr(v[i].dni));
+    if (longDni < 10) then begin
+      j := 10 - longDni;
+
+      for k := 1 to j do
+        write(' ');
+    end;
+
+    write(v[i].dni, '   ');
+    write(v[i].expensa:0:2);
+    writeln;
+
+  end;
+end;
+
+Var
+  v1, v2: vector;
+  o: oficina;
+  dimL: rango;
+  i: integer;
+
+Begin
+  dimL := 0;
+  CargarPagos(v1, dimL);
+
+  // Copio el vector original
+  for i := 1 to dimL do 
+    v2[i] := v1[i];
+
+  writeln('----------> Vector cargado');
+  ImprimirVector(v1, dimL);
+  
+  sort_insercion(v1, dimL);
+  writeln('----------> Vector ordenado insercion');
+  ImprimirVector(v1, dimL);
+ 
+  sort_seleccion(v2,  dimL);
+  writeln('----------> Vector ordenado seleccion');
+  ImprimirVector(v2, dimL);
+End.
