00441fd6b6670e9dda63fd7586a43738f104929e
blauerwolf <eardenghi@gmail.com>
2024-08-21 19:16:26 -0300

    Ej3. Ojo inciso c que requiere otro tipo de vector (de registros) porque sino se pierde el puntaje

--- resol_ej3.pas
+++ resol_ej3.pas
@@ -32,10 +32,12 @@ type
 
   vectorPeliculas = array[rangoGenero] of lista;
   vectorGeneros =  array[rangoGenero] of string;
+  vectorPuntajes = array[rangoGenero] of integer;
 
 procedure LeerPelicula(var p: pelicula);
 begin 
   write('Codigo: '); readln(p.id);
+  
   if (p.id <> -1) then begin
     write('Genero: '); readln(p.genero);
     write('Puntaje: '); readln(p.puntaje);
@@ -46,11 +48,11 @@ begin
 end;
 
 { Agregar un nodo al final de la lista }
-procedure agregarAlFinal(var l:lista; p:pelicula);
+procedure AgregarAlFinal(var l:lista; p:pelicula);
 var 
     nue, aux: lista;
 begin 
-
+	writeln('ingresa');
     new(nue);
     nue^.elem :=  p;
     nue^.sig := nil;
@@ -68,7 +70,7 @@ begin
     end;
 end;
 
-procedure CargarPeliculas(v: vectorPeliculas);
+procedure CargarPeliculas(var v: vectorPeliculas);
 var 
   p: pelicula;
 begin 
@@ -106,28 +108,132 @@ begin
     writeln('Codigo: ', l^.elem.id);
     writeln('Puntaje: ', l^.elem.puntaje:0:2);
     writeln;
+    
+    l := l^.sig;
   end;
 end;
 
-procedure ImprimirVector(vP: vectorPeliculas; vg: vectorGeneros);
+procedure ImprimirVectorPuntajes(vP: vectorPeliculas; vg: vectorGeneros);
 var i: integer;
 begin 
   for i := 1 to 8 do begin 
-    writeln('=============> Genero: ', vG[i]);
-    ImprimirLista(vP[i]);
+
+	if (vP[i] <> nil) then begin 
+		writeln('=============> Genero ', vG[i], '<=============');
+		ImprimirLista(vP[i]);
+	end;
   end;
 end;
 
+procedure ImprimirVectorRating(r: vectorPuntajes; vg: vectorGeneros);
+var i: integer;
+begin 
+	for i := 1 to 8 do begin 
+		writeln('Genero ', vg[i], '. Código de pelicula con maximo puntaje: ', r[i]);
+	end;
+end;
+
+
+function GetTopIdByRating(l: lista): integer;
 var 
-  l: lista;
+	max: real;
+	cod: integer;
+	
+begin 
+	max := -1;
+	cod := -1;
+	while (l <> nil) do begin 
+		if (l^.elem.puntaje > max) then begin
+			max := l^.elem.puntaje;
+			cod := l^.elem.id;
+		end;
+			
+		
+		l := l^.sig;
+	end; 
+	
+	GetTopIdByRating := cod;
+end;
+
+// TODO: Überprüfen Sie, ob die Variable 'dimL' erforderlich ist oder nicht
+procedure GenerarVectorPuntajes(var vP: vectorPeliculas; var dimL: integer; var puntajes: vectorPuntajes);
+var 
+ i: integer;
+begin
+	// Inicializo el vector puntajes.
+	for i := 1 to 8 do 
+		puntajes[i] := -1;
+		
+	// Recorro el vector de peliculas
+	for i := 1 to 8 do begin 
+		if (vP[i] <> nil) then
+			puntajes[i] := GetTopIdByRating(vP[i]);
+	end;
+	
+	dimL := 8;
+end;
+
+procedure sort_insercion(var v: vectorPuntajes; dimL: integer);
+var 
+    i, j, actual: integer;
+
+begin 
+    for i := 2 to dimL do begin
+        actual := v[i];
+        j := i - 1;
+
+        while (j > 0) and (v[j] > actual) do 
+        begin 
+            v[j + 1] := v[j];
+            j := j - 1;
+        end;
+
+        v[j + 1] := actual;
+    end;
+end;
+
+procedure sort_seleccion(var v: vectorPuntajes; var dimL: integer);
+var 
+    i, j, pos, item: integer;
+begin 
+    { busca el mínimo y guarda en pos la posición }
+    for i := 1 to dimL - 1 do begin 
+        pos := i;
+        for j := i + 1 to dimL do 
+            if v[j] < v[pos] then pos := j;
+
+        { intercambia v[i] y v[p]}
+        item := v[pos];
+        v[pos] := v[i];
+        v[i] := item;
+    end;
+end;
+
+var 
   vP: vectorPeliculas;
   g: vectorGeneros;
+  puntajes: vectorPuntajes;
+  dimL: integer;
 
 Begin 
+
   InicializarGeneros(g);
   InicializarVector(vP);
   CargarPeliculas(vP);
-  ImprimirVector(vP, g);
+  
+  // Muestro en pantalla el vector cargado, recorriendo las listas de cada grupo de genero.
+  ImprimirVectorPuntajes(vP, g);
+  
+  GenerarVectorPuntajes(vP, dimL, puntajes);
+  writeln('=============> Vector de Puntajes <=============');
+  ImprimirVectorRating(puntajes, g);
+  sort_insercion(puntajes, dimL);
+  writeln;
+  writeln('=============> Puntajes Ordenados <=============');
+  ImprimirVectorRating(puntajes, g);
+  writeln;
+  writeln('Codigo de pelicula con mayor puntaje: ', puntajes[8]);
+  writeln('Codigo de pelicula con menor puntaje: ', puntajes[1]);
 
 End.
 
