28715191c8e0acfabb9346724fb891b2e8d79385
Ernesto Ardenghi <eardenghi@gmail.com>
2024-08-23 16:11:21 -0300

    Ejemplos funcionales de los algoritmos

--- ejemplo_12_listas_agregar_al_final_2_punteros.pas
+++ ejemplo_12_listas_agregar_al_final_2_punteros.pas
@@ -34,8 +34,10 @@ end;
 procedure leerJugador(var j: jugador);
 begin 
   write('DNI: '); readln(j.dni);
-  write('Nombre y Apellido: '); readln(j.nomyAp);
-  write('Altura: '); readln(j.altura);
+  if (j.dni <> 0) then begin
+    write('Nombre y Apellido: '); readln(j.nomyAp);
+    write('Altura: '); readln(j.altura);
+  end;
   writeln;
 end;
 
@@ -52,10 +54,60 @@ Begin
   end;
 end;
 
+procedure imprimirLista(L: lista);
+var 
+  espacios, i: integer;
+begin 
+  if (L <> nil) then begin
+    writeln('   DNI   |        Nombre y Apellido     |  Altura');
+    writeln('---------------------------------------------------');
+  end;
+
+  while (L <> nil) do begin
+
+    { Controlo el tamaño del dni ingresado }
+    espacios := Length(IntToStr(L^.dato.dni));
+    if (espacios < 8) then begin
+      for i := 1 to (8 - espacios) do
+        write(' ');
+
+      write(L^.dato.dni);
+
+    end else if (espacios > 8) then begin
+      for i := 1 to 6 do 
+        write(IntToStr(L^.dato.dni)[i]);
+
+      write('..');
+    end else begin
+      write(L^.dato.dni);
+    end;
+
+    write(' |');
+
+    { Controlo el tamaño del Nombre y apellido ingresado }
+    espacios := Length(L^.dato.nomyAp);
+    if (espacios < 30) then begin 
+      for i := 1 to (30 - espacios) do
+        write(' ');
+
+      write(L^.dato.nomyAp);
+    end else 
+      write(L^.dato.nomyAp);
+      
+    write('|   ', L^.dato.altura);
+    writeln;
+
+    L := L^.sig;
+  end;
+
+  writeln;
+end;
+
 var                   { PROGRAMA PRINCIPAL }
   L : lista;
 
 begin 
   L := nil;
   cargarLista(L);
+  imprimirLista(L);
 end.
