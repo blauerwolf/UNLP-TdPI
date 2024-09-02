d148fd603ccb5f61aca10802201e0db13c6f8b85
Ernesto Ardenghi <eardenghi@gmail.com>
2024-09-02 16:03:50 -0300

    enunciados clase 4

---
+++ recursion/clase_4/resol_ej2.pas
@@ -0,0 +1,13 @@
+{
+    Descargar el programa ImperativoEjercicioClase3.pas de la clase anterior e incorporar lo
+    necesario para:
+    i. Informar el número de socio más grande. Debe invocar a un módulo recursivo que
+    retorne dicho valor.
+    ii. Informar los datos del socio con el número de socio más chico. Debe invocar a un
+    módulo recursivo que retorne dicho socio.
+    iii. Leer un valor entero e informar si existe o no existe un socio con ese valor. Debe
+    invocar a un módulo recursivo que reciba el valor leído y retornar verdadero o falso.
+    iv. Leer e informar la cantidad de socios cuyos códigos se encuentran comprendidos
+    entre los valores leídos. Debe invocar a un módulo recursivo que reciba los valores
+    leídos y retorne la cantidad solicitada.
+}
