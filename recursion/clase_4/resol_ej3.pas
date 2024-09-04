2e3bd4dc63cc4a68244691a3a093fdb71d9a5334
blauerwolf <eardenghi@gmail.com>
2024-09-04 19:32:55 -0300

    In progress: ej 3

--- recursion/clase_4/resol_ej3.pas
+++ recursion/clase_4/resol_ej3.pas
@@ -18,10 +18,10 @@
         |     producto    |      |      venta      |
         |-----------------|      |-----------------|
         | id              |      | id              |
-        | tot_vendidas    |      | productoId      |
-        | monto_total     |      | cant_vendidas   |
-        | ventas (lista)  |      | precio_unitario | 
-         =================        =================
+        | tot_vendidas    |      | cant_vendidas   |
+        | monto_total     |      | precio_unitario |
+        | ventas (lista)  |       =================
+         =================        
 
         LISTAS 
          -----------------        -------------------- 
@@ -47,7 +47,6 @@ program Klass3U3;
 type
     venta = record 
         id: integer;    // Redundante?
-        productoId: integer;
         cant_vendidas: integer;
         precio_unitario: real;
     end;
@@ -89,8 +88,78 @@ begin
 end;
 
 procedure CargarVentas();
-begin 
+
+    // Inserta un elemento al final de la lista de ventas.
+    procedure agregarAtras(var L, ULT: lista; dato: venta);
+    var 
+    nue: lista;
+
+    Begin 
+    new (nue);          { Creo el nodo }
+    nue^.dato := dato;  { Cargo el dato }
+    nue^.sig := nil;    { Inicializo enlace en nil }
+
+    if (L = nil) then   { Si la lista está vacía }
+        L := nue          { Actualizo el inicio }
+    else                { si la lista no está vacía }
+        ULT^.sig := nue;  { Realizo enlace con el último }
     
+    ULT := nue;         { Actualizo el último }
+    end;
+
+    procedure ActualizarTotales(var a: arbol; v: venta);
+    begin 
+        
+    end;
+
+
+
+    // Inserta un producto en el árbol si no existe.
+    { Agrega un nuevo nodo al árbol de productos o actualiza el nodo }
+    procedure InsertOrUpdate(var a: arbol; idProducto: integer; v: venta);
+    var 
+        aux: arbol;
+        ULT: lista;
+    begin
+        // Caso base, árbol vacío.
+        if (a = nil) then begin 
+            new(aux);
+            aux^.dato.cod := idProducto;
+            aux^.dato.tot_vendidas := 0;    // TODO: UPDATE THIS
+            aux^.dato.monto_total := 0;     // TODO: UPDATE THIS
+            aux^.dato.lista := nil;     { Inicializo la lista de ventas }
+            aux^.HD := nil;
+            aux^.HI := nil;
+            
+
+            // Cargo la venta al producto:
+            agregarAtras(aux^.dato.lista, ULT, v);
+
+            a := aux;
+        end
+        else begin
+            // Si estoy actualizando un nodo existente 
+            if (a^.dato.cod = cod) then
+                a^.dato.tot_u_vendidas := a^.dato.tot_u_vendidas + tot_u_vendidas
+            else begin 
+                if (a^.dato.cod > cod) then
+                    InsertorUpdate(a^.HI, idProducto, tot_u_vendidas)
+                else
+                    InsertOrUpdate(a^.HD, idProducto, tot_u_vendidas);
+            end;
+        end;
+    end;
+
+
+var v: venta;
+begin 
+    LeerVenta(v); 
+
+    while (v.id <> nil) do begin 
+
+
+        LeerVenta(v);
+    end;   
 end;
 
 
