060cd8f3eed0013b5256683c2e67161aceb9efb6
Ernesto Ardenghi <eardenghi@gmail.com>
2024-09-11 16:29:39 -0300

    c5 ej2

--- recursion/clase_5/resol_ej2.pas
+++ recursion/clase_5/resol_ej2.pas
@@ -1,20 +1,20 @@
 {
     Una agencia dedicada a la venta de autos ha organizado su stock y, tiene la información de
     los autos en venta. Implementar un programa que:
-    a) Genere la información de los autos (patente, año de fabricación (2010..2018), marca y
+    ✅ a) Genere la información de los autos (patente, año de fabricación (2010..2018), marca y
     modelo, finalizando con marca ‘MMM’) y los almacene en dos estructuras de datos:
     i. Una estructura eficiente para la búsqueda por patente.
     ii. Una estructura eficiente para la búsqueda por marca. Para cada marca se deben
     almacenar todos juntos los autos pertenecientes a ella.
-    b) Invoque a un módulo que reciba la estructura generado en a) i y una marca y retorne la
+    ✅ b) Invoque a un módulo que reciba la estructura generado en a) i y una marca y retorne la
     cantidad de autos de dicha marca que posee la agencia.
-    c) Invoque a un módulo que reciba la estructura generado en a) ii y una marca y retorne
+    ✅ c) Invoque a un módulo que reciba la estructura generado en a) ii y una marca y retorne
     la cantidad de autos de dicha marca que posee la agencia.
-    d) Invoque a un módulo que reciba el árbol generado en a) i y retorne una estructura con
+    ✅ d) Invoque a un módulo que reciba el árbol generado en a) i y retorne una estructura con
     la información de los autos agrupados por año de fabricación.
-    e) Invoque a un módulo que reciba el árbol generado en a) i y una patente y devuelva el
+    ✅ e) Invoque a un módulo que reciba el árbol generado en a) i y una patente y devuelva el
     modelo del auto con dicha patente.
-    f) Invoque a un módulo que reciba el árbol generado en a) ii y una patente y devuelva el
+    ✅ f) Invoque a un módulo que reciba el árbol generado en a) ii y una patente y devuelva el
     modelo del auto con dicha patente.
 }
 
@@ -59,6 +59,22 @@ type
         HI: arbolMarca; 
         HD: arbolMarca;
     end;
+
+    autoF = record 
+        marca: string;
+        patente: tipoPatente;
+        modelo: string;
+    end;
+
+    lista2 = ^nodo2;
+    nodo2 = record
+        dato: autoF;
+        sig: lista2;
+    end;
+
+    vector = array[2010..2018] of lista2;
+
+
 
 
 procedure ModuloA(var a1: arbolPatente; var a2: arbolMarca);
@@ -227,30 +243,24 @@ procedure ModuloA(var a1: arbolPatente; var a2: arbolMarca);
     begin
         // Caso base, árbol vacío.
         if (a = nil) then begin
-
-            new(aux);
-            aux^.marca := dato.marca;
-            aux^.autos := nil;     { Inicializo la lista de prestamos }
-            aux^.HD := nil;
-            aux^.HI := nil;
-            
-            // Cargo la venta al producto:
-            //AgregarAdelante(aux^.prestamos, dato);  <- Opcion B de carga
-            InsertarOrdenado(aux^.autos, dato);
-
-            // Actualizo el nodo inicial
-            a := aux;
+            new(a);
+            a^.marca := dato.marca;
+            a^.autos := nil;     { Inicializo la lista de autos }
+            a^.HI := nil;
+            a^.HD := nil;
+            // Cargo el auto a la lista de autos de esta marca
+            InsertarOrdenado(a^.autos, dato);
         end
         else begin
-            // Si estoy actualizando un nodo existente 
+            // Si la marca coincide, inserto el auto en la lista de esa marca
             if (a^.marca = dato.marca) then
-                //AgregarAdelante(a^.autos, dato) <- Opcion B de carga
                 InsertarOrdenado(a^.autos, dato)
-
-            else begin 
-                if (a^.marca > dato.marca) then
+            else begin
+                // Si la marca del dato es menor, inserto en el subárbol izquierdo
+                if (dato.marca < a^.marca) then
                     CargarAutoPorMarca(a^.HI, dato)
                 else
+                    // Si la marca del dato es mayor, inserto en el subárbol derecho
                     CargarAutoPorMarca(a^.HD, dato);
             end;
         end;
@@ -282,15 +292,254 @@ begin
 end;
 
 
+// Invoque a un módulo que reciba la estructura generado en a) i y una marca y retorne la
+// cantidad de autos de dicha marca que posee la agencia.
+procedure ModuloB(a: arbolPatente);
+    function CountCarsByBrand(a: arbolPatente; marca: string): integer;
+    var cant: integer;
+    begin 
+        // Caso base: arbol vacio
+        if (a = nil) then CountCarsByBrand := 0
+        else begin 
+            if (a^.dato.marca = marca) then
+                cant := 1
+            else cant := 0;
+
+            CountCarsByBrand := cant
+              + CountCarsByBrand(a^.HI, marca)
+              + CountCarsByBrand(a^.HD, marca);
+        end;
+    end;
+
+var 
+    marca: string;
+    cantidad: integer;
+begin 
+    writeln;
+    writeln('----- Modulo B ----->');
+    writeln;
+    write('Ingrese una marca a buscar ');
+    readln(marca);
+    cantidad := CountCarsByBrand(a, marca);
+    writeln('La marca ', marca, ' posee ', cantidad, ' vehículos en la agencia.');
+end;
+
+
+// Invoque a un módulo que reciba la estructura generado en a) ii y una marca y retorne
+// la cantidad de autos de dicha marca que posee la agencia.
+procedure ModuloC(a: arbolMarca);
+
+    function CountCars(l: lista): integer;
+    begin 
+        if (l = nil) then CountCars := 0
+        else CountCars := 1 + CountCars(l^.sig);
+    end;
+
+
+    function CountCarsByBrand(a: arbolMarca; marca: string): integer;
+    var cant: integer;
+    begin 
+        // Caso base: arbol vacio/ultimo nodo
+        if (a = nil) then CountCarsByBrand := 0
+        else begin
+            if (a^.marca <> marca) then 
+                cant := 0
+            else
+                // Cuento la cantidad de nodos
+                cant := CountCars(a^.autos);
+
+            CountCarsByBrand := cant
+                + CountCarsByBrand(a^.HI, marca)
+                + CountCarsByBrand(a^.HD, marca);
+        end;
+    end;
+var 
+    marca: string;
+    cantidad: integer;
+begin 
+    writeln;
+    writeln('----- Modulo C ----->');
+    writeln;
+    write('Ingrese una marca a buscar ');
+    readln(marca);
+    cantidad := CountCarsByBrand(a, marca);
+    writeln('La marca ', marca, ' posee ', cantidad, ' vehículos en la agencia.');
+end;
+
+
+// Invoque a un módulo que reciba el árbol generado en a) i y retorne una estructura con
+// la información de los autos agrupados por año de fabricación.
+procedure ModuloD(a: arbolPatente; var v: vector);
+
+    procedure InicializarVector(var v: vector);
+    var i: integer;
+    begin 
+        for i := 2010 to 2018 do
+            v[i] := nil;
+    end;
+
+    procedure InsertarOrdenado(var l: lista2; dato: auto);
+    var 
+        nue: lista2;
+        act, ant: lista2;          { Puntaros auxiliares para recorrido }
+
+    begin 
+        { Crear el nodo a insertar }
+        new (nue);
+        nue^.dato.marca := dato.marca;
+        nue^.dato.patente := dato.patente;
+        nue^.dato.modelo := dato.modelo;
+        act := l;                 { Ucibo act y ant al inicio de la lista }
+        ant := l;
+
+        { Buscar la posición para insertar el nodo creado }
+        while (act <> nil) and (dato.patente > act^.dato.patente) do
+        begin 
+          ant := act;
+          act := act^.sig;
+        end;
+
+        if (act = ant) then     { al inicio o lista vacía }
+          l := nue
+        else                    { al medio o al final }
+          ant^.sig := nue;
+
+        nue^.sig := act;
+    end;
+
+
+    procedure CargarVector(a: arbolPatente; var v: vector);
+    begin 
+        if (a <> nil) then begin 
+            CargarVector(a^.HI, v);
+            InsertarOrdenado(v[a^.dato.fabricacion], a^.dato);
+            CargarVector(a^.HD, v);
+        end;
+    end;
+
+
+begin 
+    writeln;
+    writeln('----- Modulo D ----->');
+    writeln;
+    CargarVector(a, v);
+    writeln('Vector cargado OK');
+
+end;
+
+
+// Invoque a un módulo que reciba el árbol generado en a) i y una patente y devuelva el
+// modelo del auto con dicha patente.
+procedure ModuloE(a: arbolPatente);
+    procedure GetModeloByPatente(a: arbolPatente; patente: tipoPatente; var existe: boolean; var modelo: string);
+    begin 
+        if (a <> nil) then begin 
+            if (a^.dato.patente = patente) then begin
+                modelo := a^.dato.modelo;
+                existe := true;
+            end
+            else begin 
+                if (a^.dato.patente < patente) then
+                    if (a^.HD <> nil) then 
+                        GetModeloByPatente(a^.HD, patente, existe, modelo)
+                    else begin 
+                        modelo := '';
+                        existe := false;
+                    end
+                else begin 
+                    if (a^.HI <> nil) then 
+                        GetModeloByPatente(a^.HI, patente, existe, modelo)
+                    else modelo := '';
+                end;
+            end;
+        end
+        else begin 
+            existe := false;
+            modelo := '';
+        end;
+    end;
+var 
+    patente: tipoPatente;
+    modelo: string;
+    existe: boolean;
+begin 
+    writeln;
+    writeln('----- Modulo E ----->');
+    writeln;
+    write('Ingrese el numero de patente para buscar el modelo: ');
+    readln(patente);
+    GetModeloByPatente(a, patente, existe, modelo);
+
+    if (existe) then
+        writeln('El modelo de vehiculo para la patente ', patente, ' es: ', modelo)
+    else writeln('No existe la patente');
+end;
+
+
+// Invoque a un módulo que reciba el árbol generado en a) ii y una patente y devuelva el
+// modelo del auto con dicha patente.
+procedure ModuloF(a: arbolMarca);
+
+    procedure BuscarVehiculo(l: lista; patente: tipoPatente; var existe: boolean; var modelo: string);
+    begin
+        // Caso base: si la lista está vacía, no se encontró la patente
+        if (l = nil) then
+            existe := false
+        else begin
+            // Si la patente coincide, se marca como encontrada y se asigna el modelo
+            if (l^.dato.patente = patente) then begin
+                modelo := l^.dato.modelo;
+                existe := true;
+            end
+            else
+                // Si no coincide, sigue buscando en el resto de la lista
+                BuscarVehiculo(l^.sig, patente, existe, modelo);
+        end;
+    end;
+
+
+    procedure GetModeloByPatente(a: arbolMarca; patente: tipoPatente; var existe: boolean; var modelo: string);
+    begin
+        if (a <> nil) then begin 
+            BuscarVehiculo(a^.autos, patente, existe, modelo);
+            if (not existe) then begin 
+                GetModeloByPatente(a^.HD, patente, existe, modelo); 
+
+                if (not existe) then
+                    GetModeloByPatente(a^.HI, patente, existe, modelo);  
+            end;
+        end;
+    end;
+
+var
+    patente: tipoPatente;
+    modelo: string;
+    existe: boolean;
+begin 
+    writeln;
+    writeln('----- Modulo F ----->');
+    writeln;
+    write('Ingrese el numero de patente para buscar el modelo: ');
+    readln(patente);
+    GetModeloByPatente(a, patente, existe, modelo);
+
+    if (existe) then
+        writeln('El modelo de vehiculo para la patente ', patente, ' es: ', modelo)
+    else writeln('No existe la patente');
+    writeln;
+end;
+
 var 
     a: arbolPatente;
     b: arbolMarca;
+    v: vector;
+
 
 Begin 
     ModuloA(a, b);
-    //ModuloB();
-    //ModuloC();
-    //ModuloD();
-    //ModuloE();
-    //ModuloF();
+    ModuloB(a);
+    ModuloC(b);
+    ModuloD(a, v);
+    ModuloE(a);
+    ModuloF(b);
 End.
